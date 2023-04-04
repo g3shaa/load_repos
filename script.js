d7760fff1ef4e0632033f9da3b2cf259d7d2559f
Geshata <64840882+g3shaa@users.noreply.github.com>
2023-04-04 10:36:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 function FuncRepos() {
-    $('#repos').empty();
-    let url = "https://api.github.com/users/" + $("#username").val() + "/repos";
-    return $.ajax({
-        url: url,
-        success: displayRepos,
-        error: displayError
-    });
-        
+// Изчистваме списъка с хранилищата
+$('#repos').empty();
+
+// Съставяме URL адреса за заявката, като използваме стойността в полето за въвеждане на потребителското име
+let url = "https://api.github.com/users/" + $("#username").val() + "/repos";
+
+// Изпращаме заявка към GitHub API и връщаме Promise обект
+return $.ajax({
+url: url,
+success: displayRepos, // Извикваме функцията за показване на хранилищата, ако заявката е успешна
+error: displayError // Извикваме функцията за показване на грешката, ако заявката е неуспешна
+});
 }
 
-//функция ,която показва хранилището  като <li> таг и като хипервръзка 
+// Функция, която показва хранилищата като списък с хипервръзки
 function displayRepos(repos) {
-    for (let repo of repos) {
-        let link = $('<a>').text(repo.full_name).attr('href', repo.html_url);
-        $('#repos').append($('<li>').append(link));
-    }
+for (let repo of repos) {
+let link = $('<a>').text(repo.full_name).attr('href', repo.html_url);
+$('#repos').append($('<li>').append(link));
+}
 }
 
-//функция ,ако няма хранилище с име ,което е въведено от потребителя във формата
+// Функция, която показва грешката, ако няма хранилище с името, въведено от потребителя
 function displayError(err) {
-    $('#repos').append($('<li>').text('Грешка: Виж дали си написал правилно името!'));
-
+$('#repos').append($('<li>').text('Грешка: Виж дали си написал правилно името!'));
 }
